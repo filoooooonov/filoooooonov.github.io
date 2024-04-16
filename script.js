750513a0cffb6808738d88ccb7072cbacac7c8a0
Aleksei Filonov <alexfiloonov@gmail.com>
2024-04-16 22:32:15 +0300

    Commit

--- script.js
+++ script.js
@@ -57,3 +57,36 @@ function copyToClipboard(text, element) {
         element.textContent = text;
     }, 3000); // Change back to the original text after 2 seconds
 }
+
+const sections = document.querySelectorAll("section")
+
+sections.forEach((section) => {
+    window.addEventListener("load", eventListener);
+    window.addEventListener("scroll", eventListener);
+        
+    function eventListener() {
+        let windowHeight = window.innerHeight;
+        let sectionRectTop = section.getBoundingClientRect().top;
+
+
+        // console.log("Window Height: " + windowHeight);
+        // console.log("Section RectTop: " + sectionRectTop)
+        if(sectionRectTop < windowHeight) {
+            section.classList.add("active");
+        }
+    }
+
+    // revealing elements one by one
+    window.addEventListener("scroll", () => {
+        let reveals = section.querySelectorAll(".reveal");
+
+        reveals.forEach((reveal, index) => {
+            if(section.classList.contains("active")) {
+                const delay = 600;
+                setTimeout(() => {
+                    reveal.classList.add("active");
+                }, index * delay)
+            }
+        });
+    });
+});
