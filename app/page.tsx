bec1c151d84fe8ca1c75b56e4dd5745c44079e33
Aleksei Filonov <alexfiloonov@gmail.com>
2024-07-16 12:09:54 +0300

    Test deploy to pages

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
     <main className="max-w-[1024px] mx-auto px-6 md:px-4 lg:px-0 pb-40">
-      <div></div>
+      <div>TEST</div>
       <Hero />
       <MyProjects />
       <Skills />
