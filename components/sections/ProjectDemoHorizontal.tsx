2031eff9b3b375785ebfb68c0ac66d0c590ef3f6
Aleksei Filonov <alexfiloonov@gmail.com>
2024-10-12 18:10:18 +0300

    Add links to projects

--- components/sections/ProjectDemoHorizontal.tsx
+++ components/sections/ProjectDemoHorizontal.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 import { Project } from "@/components/sections/ProjectList";
+import { RxOpenInNewWindow } from "react-icons/rx";
 
 interface ProjectDemoHorizontalProps {
   project: Project;
@@ -9,14 +10,23 @@ interface ProjectDemoHorizontalProps {
 
 const ProjectDemoHorizontal = ({ project }: ProjectDemoHorizontalProps) => {
   return (
-    <div className="group flex flex-col sm:grid grid-cols-5 mb-12 cursor-pointer">
+    <div className="group flex flex-col sm:grid grid-cols-5 mb-12">
       <div className="col-span-3">
         <h3 className="mb-2 group-hover:text-primary transition duration-200">
-          {project.title}
+          <a
+            href={project.link}
+            target="_blank"
+            className={`flex gap-4 items-center ${
+              project.link && "cursor-pointer"
+            }`}
+          >
+            {project.title}
+            {project.link && <RxOpenInNewWindow />}
+          </a>
         </h3>
 
         <p>{project.descriptionShort}</p>
-        <div className="flex gap-2 mt-6 flex-wrap">
+        <div className="flex gap-2 mt-6 flex-wrap mb-4">
           {project.tags.map((tag, i) => {
             return (
               <div key={i} className="tag">
@@ -26,6 +36,7 @@ const ProjectDemoHorizontal = ({ project }: ProjectDemoHorizontalProps) => {
           })}
         </div>
       </div>
+
       <div className="col-span-2 sm:pl-8">
         <Image
           src={project.image}
