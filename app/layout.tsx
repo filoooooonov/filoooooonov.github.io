bc6e2afe2289d19c33d10a0ccb172f659b4c46ca
Aleksei Filonov <alexfiloonov@gmail.com>
2025-01-04 18:38:12 +0200

    Add consent to gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
+            gtag('consent', 'default', {
+                  'analytics_storage': 'denied',
+                  'ad_storage': 'denied',
+                  'ad_user_data': 'denied',
+                  'ad_personalization': 'denied',
+                  'personalization_storage': 'denied',
+                  'functionality_storage': 'granted',
+                  'security_storage': 'granted',
+                });
+
             gtag('config', 'G-WG3RFFPKQ7');
             `}
         </Script>
